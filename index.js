2b9e6c781a326885d1427efd29156d22acc40cf7
Nitish Nashine <nitishnashine@gmail.com>
2024-10-26 20:29:21 +0530

    Refactor CORS configuration in app.use() and update CORS origin and configuration in app.use()

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db'); // Make sure to define your MongoDB connection in this file
+const taskRoutes = require('./routes/taskRoutes');
+const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+connectDB();
+
 // CORS Configuration
 const corsOptions = {
-    origin: '*', 
+    origin: 'https://taskmagementclient.vercel.app', // Change to your client URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
 
 // Use CORS middleware
 app.use(cors(corsOptions));
+
+// Middleware
 app.use(bodyParser.json());
+
+// Log incoming requests
+app.use((req, res, next) => {
+    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+    next();
+});
 
 // Health check route
 app.get('/', (req, res) => {    
     res.send('Hello World');
 });
 
-// Preflight OPTIONS request
+// Enable pre-flight for all routes
 app.options('*', cors(corsOptions));
 
-// Define your routes here
+// Routes
+app.use('/api', taskRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
 
 // Start Server
 app.listen(PORT, () => {
