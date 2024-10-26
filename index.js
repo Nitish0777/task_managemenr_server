4b535ac221bf2d8d9846d5cdec28d5cc98f08c5e
Nitish Nashine <nitishnashine@gmail.com>
2024-10-26 20:17:17 +0530

    Refactor CORS configuration in app.use()

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db'); 
-const taskRoutes = require('./routes/taskRoutes');
-const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-connectDB();
-
+// CORS Configuration
 const corsOptions = {
-    origin: '*',
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
 
 // Use CORS middleware
 app.use(cors(corsOptions));
-
-// Middleware
 app.use(bodyParser.json());
-
-// Log incoming requests
-app.use((req, res, next) => {
-    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-    next();
-});
 
 // Health check route
 app.get('/', (req, res) => {    
     res.send('Hello World');
 });
 
+// Preflight OPTIONS request
 app.options('*', cors(corsOptions));
 
-// Routes
-app.use('/api', taskRoutes);
+// Define your routes here
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
