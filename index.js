cb5ee6e75c49703aac1e8c01fa456a982112e27b
Nitish Nashine <nitishnashine@gmail.com>
2024-10-26 20:07:07 +0530

    Update CORS origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 
 // CORS Configuration
 const corsOptions = {
-    origin: 'https://taskmagementclient.vercel.app',
+    origin: 'https://taskmagementclient.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
