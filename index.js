da2bcd9886046f62186ef3a79b6f003cfaf8e74e
Nitish Nashine <nitishnashine@gmail.com>
2024-10-26 19:46:50 +0530

    Update CORS origin in app.use()

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Cors
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://taskmagementclient.vercel.app/",
+    }
+    
+));
 
 // Middleware
 app.use(bodyParser.json());
