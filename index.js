547add77313deee69b761cca993b1019c489c8bc
Nitish Nashine <nitishnashine@gmail.com>
2024-10-26 20:01:11 +0530

    Update CORS origin and configuration in app.use()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); // Make sure to define your MongoDB connection in this file
 const taskRoutes = require('./routes/taskRoutes');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
@@ -14,21 +14,30 @@ connectDB();
 
 // CORS Configuration
 const corsOptions = {
-    origin: 'https://taskmagementclient.vercel.app', 
+    origin: 'https://taskmagementclient.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    credentials: true,
 };
 
 // Use CORS middleware
 app.use(cors(corsOptions));
 
-
 // Middleware
 app.use(bodyParser.json());
 
+// Log incoming requests
+app.use((req, res, next) => {
+    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+    next();
+});
+
+// Health check route
 app.get('/', (req, res) => {    
     res.send('Hello World');
 });
+
+app.options('*', cors(corsOptions));
+
 // Routes
 app.use('/api', taskRoutes);
 app.use('/api/auth', authRoutes);
